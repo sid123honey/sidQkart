dca85d5bb98ffe7b761f42a9966e87d2d25db644
sid123honey <siddharthjain85075@gmail.com>
2024-10-16 17:22:18 +0530

    updated render link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Checkout from "./components/Checkout"
 import Thanks from "./components/Thanks"; 
  
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://qkart-frontend2-kpgn.onrender.com/api/v1`,
 };
 
 function App() { 
